88a1cc057d9cb925521bc1ebed816b89b601b83d
Sharad Mishra <sharadm2001@gmail.com>
2020-06-10 17:06:50 +0530

    oauth package change:

--- server/utils/passport.js
+++ server/utils/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const GitHubStrategy = require('passport-github2')
 const User = require('../models/User')
 
